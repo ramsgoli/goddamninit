763bdcdb1da31a0af5cce05851d7c37053f36d9f
Ram Goli <ramsgoli@gmail.com>
2017-07-27 06:36:40 -0700

    added templates

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var chalk = require('chalk');
-var dot = require('dot');
+#!/usr/bin/env node
+const fs = require('fs');
+const chalk = require('chalk');
+const dot = require('dot');
+const figlet = require('figlet');
+const handlebars = require('handlebars');
 
 console.log(
-        chalk.yellow("Welcome")
-        );
+    chalk.yellow(
+       figlet.textSync('Goddamninit')
+    )
+);
+
+fs.readFile('templates/webpack.config.hbs', function(err, data) {
+    if (!err) {
+        let source = data.toString()
+        let template = handlebars.compile(source)({entry: 'hello world'})
+        console.log(template)
+    } else {
+        console.log(err)
+    }
+});
+
